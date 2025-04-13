8df9a96e366458b55ea52893cba3d67a5dcb868d
NUROKU <shimau6@yahoo.co.jp>
2025-04-13 17:57:25 +0900

    無償版対応

--- PSDRexa/PSDRexaSyncer/main_ui.py
+++ PSDRexa/PSDRexaSyncer/main_ui.py
@@ -1,50 +1,45 @@
+import tkinter as tk
+from tkinter import messagebox
 from PSDRexaSyncer.BaseSyncer import BaseSyncer
 from PSDRexaSyncer.Syncer1 import Syncer1
 from PSDRexaSyncer.Syncer2 import Syncer2
 
-import tkinter as tk
-from tkinter import messagebox
-
-
 class PSDRexaSyncerUI:
     def __init__(self, fu, bmd, resolve):
         self._resolve = resolve
-        self._fu = fu
-        self._bmd = bmd
-        self._ui = fu.UIManager
-        self._dispatcher = bmd.UIDispatcher(self._ui)
 
-        elements = [
-            self._ui.Label({'Text': ""}),
+        self.root = tk.Tk()
+        self.root.title("PSDRexa mouse sync")
+        self.root.geometry("400x200")
 
-            self._ui.Label({"ID": "VideoTrackLabel", "Text": "Video Track", "Geometry": [10, 10, 150, 30]}),
-            self._ui.SpinBox({"ID": "VideoTrackInput", "Text": "", 'Value': 1, "Geometry": [120, 10, 170, 30]}),
-            self._ui.Label({"ID": "AudioTrackLabel", "Text": "Audio Track", "Geometry": [10, 50, 150, 30]}),
-            self._ui.SpinBox({"ID": "AudioTrackInput", "Text": "", 'Value': 1, "Geometry": [120, 50, 170, 30]}),
-            self._ui.Button({"ID": "Sync2Button", "Text": "口パクSync", "Geometry": [10, 90, 100, 30]}),
-            self._ui.Label({"ID": "Result2Label", "Text": "-", "Geometry": [120, 90, 150, 30]}),
-            self._ui.Button({"ID": "Sync1Button", "Text": "目パチのキーフレームを再構築", "Geometry": [10, 130, 150, 30]}),
-            self._ui.Label({"ID": "Result1Label", "Text": "-", "Geometry": [170, 130, 150, 30]}),
-        ]
+        # UIパーツ
+        tk.Label(self.root, text="Video Track").place(x=10, y=10)
+        self.video_track_input = tk.Spinbox(self.root, from_=1, to=10, width=5)
+        self.video_track_input.place(x=120, y=10)
 
-        self._window = self._dispatcher.AddWindow(
-            {'ID': 'MyWindow', 'WindowTitle': 'PSDRexa mouse sync', "Geometry": [400, 170, 400, 170]}, elements)
+        tk.Label(self.root, text="Audio Track").place(x=10, y=50)
+        self.audio_track_input = tk.Spinbox(self.root, from_=1, to=10, width=5)
+        self.audio_track_input.place(x=120, y=50)
 
-        self._window.On.Sync1Button.Clicked = self.StartSync1
-        self._window.On.Sync2Button.Clicked = self.StartSync2
-        self._window.On.MyWindow.Close = self.OnClose
-        self._window.Show()
-        self._dispatcher.RunLoop()
-        self._window.Hide()
+        self.result2_label = tk.Label(self.root, text="-")
+        tk.Button(self.root, text="口パクSync", command=self.start_sync2).place(x=10, y=90)
+        self.result2_label.place(x=120, y=90)
 
-    def OnClose(self, ev):
-        self._dispatcher.ExitLoop()
+        self.result1_label = tk.Label(self.root, text="-")
+        tk.Button(self.root, text="目パチのキーフレームを再構築", command=self.start_sync1).place(x=10, y=130)
+        self.result1_label.place(x=170, y=130)
 
-    def StartSync2(self, ev):
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.root.mainloop()
+
+    def on_close(self):
+        self.root.destroy()
+
+    def start_sync2(self):
         try:
-            self._window.Find('Result2Label').Text = f"Processing..."
-            video_index = self._window.Find('VideoTrackInput').Value
-            audio_index = self._window.Find('AudioTrackInput').Value
+            self.result2_label.config(text="Processing...")
+            video_index = int(self.video_track_input.get())
+            audio_index = int(self.audio_track_input.get())
 
             project_manager = self._resolve.GetProjectManager()
             project = project_manager.GetCurrentProject()
@@ -64,22 +59,17 @@ class PSDRexaSyncerUI:
             else:
                 message = "口パク設定が完了しましたが、一部のwavファイルの読み込みでエラーが発生しました。\n詳しくはDaVinci Resolveのコンソールを確認してください"
 
-            root = tk.Tk()
-            root.withdraw()
             messagebox.showinfo("PSDRexa_Syncer", message)
-            root.destroy()
-            self._window.Find('Result2Label').Text = f"-"
+            self.result2_label.config(text="-")
         except Exception as e:
-            root = tk.Tk()
-            root.withdraw()
-            messagebox.showinfo("PSDRexa_Syncer", f"ERROR:{str(e)}")
-            root.destroy()
-            self._window.Find('Result2Label').Text = f"-"
+            messagebox.showerror("PSDRexa_Syncer", f"ERROR: {str(e)}")
+            self.result2_label.config(text="-")
 
-    def StartSync1(self, ev):
+    def start_sync1(self):
         try:
-            self._window.Find('Result1Label').Text = f"Processing..."
-            video_index = self._window.Find('VideoTrackInput').Value
+            self.result1_label.config(text="Processing...")
+            video_index = int(self.video_track_input.get())
+
             project_manager = self._resolve.GetProjectManager()
             project = project_manager.GetCurrentProject()
             timeline = project.GetCurrentTimeline()
@@ -96,14 +86,8 @@ class PSDRexaSyncerUI:
             else:
                 message = "目パチ設定が完了しましたが、一部のwavファイルの読み込みでエラーが発生しました。\n詳しくはDaVinci Resolveのコンソールを確認してください"
 
-            root = tk.Tk()
-            root.withdraw()
             messagebox.showinfo("PSDRexa_Syncer", message)
-            root.destroy()
-            self._window.Find('Result1Label').Text = f"-"
+            self.result1_label.config(text="-")
         except Exception as e:
-            root = tk.Tk()
-            root.withdraw()
-            messagebox.showinfo("PSDRexa_Syncer", f"ERROR:{str(e)}")
-            root.destroy()
-            self._window.Find('Result2Label').Text = f"-"
+            messagebox.showerror("PSDRexa_Syncer", f"ERROR: {str(e)}")
+            self.result1_label.config(text="-")
